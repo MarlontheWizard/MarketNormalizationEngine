3c71bd186c24fdb4423b5c1e32311ee608d00062
Marlon Dominguez <marlon.dominguez307@gmail.com>
2026-05-11 01:52:59 -0400

    Implemented parallel, or multithreaded, downloads. Updated logging to use progress bar for each days worth of data downloaded and removed comprehensive print statements. Added location argument to cli so that users can specify where to save data (default is bi5_data folder in current directory). Moved driver to main().

--- Dukascopy/Manual_Data_Layer/dukascopy_data_downloader.py
+++ Dukascopy/Manual_Data_Layer/dukascopy_data_downloader.py
@@ -4,10 +4,12 @@ from zipfile import ZipFile
 from io import BytesIO
 import argparse
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-OUTPUT_DIR = os.path.join(BASE_DIR, "bi5_data")
 
+#Server location
 BASE_URL = "https://datafeed.dukascopy.com/datafeed"
 
 def build_filename(symbol: str,
@@ -37,7 +39,7 @@ def build_download_url(symbol: str,
     )
 
 
-def download_file(url):
+def fetch_data_from_server(url):
     
     r = requests.get(url, stream=True)
     if r.status_code != 200:
@@ -52,37 +54,67 @@ def extract_zip(zip_bytes, output_dir):
         z.extractall(output_dir)
 
 
-def download_day(symbol, year, month, day, output_dir="bi5_data"):
-
-    for hour in range(0, 24, 1):     
+def download_hour_data(symbol, year, month, day, hour, output_dir):
+ 
         
-        url = build_download_url(symbol, year, month, day, hour)
-        print(f"[INFO] Downloading: {url}")
-        data = download_file(url)
+    url = build_download_url(symbol, year, month, day, hour)
+    
+    #print(f"[INFO] Downloading: {url}")
+    
+    data = fetch_data_from_server(url)
         
-        if data is None:
+    if data is None:
             
-            print("[WARNING] No data for this date.")
+        print(f"[WARNING] No data for hour {hour} on {year}-{month}-{day}")    
+
+        return
+
+    file_name = build_filename(symbol, year, month, day, hour)
+
+    file_path = os.path.join(output_dir, file_name)
+    
+    open(file_path, "wb").write(data)
+
+    #print(f"[SUCCESSFUL] Completed hour {hour}. Fetched data saved to {dir}")
+
+    
+#Dukascopy server only provides data in hourly timeframe
+def download_day_data(symbol, year, month, day, output_dir, threads=4):
+
+    hours = list(range(24))
+
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+
+        futures = [
+            executor.submit(
+                download_hour_data,
+                symbol,
+                year,
+                month,
+                day,
+                hour,
+                output_dir
+            )
             
-            continue
+            for hour in hours
+        ]
 
-        dir = os.path.join(
-            output_dir,
-            symbol,
-            f"{year}-{month:02d}-{day:02d}"
-        )
+        results = []
 
-        os.makedirs(dir, exist_ok=True)
+        with tqdm(total=24, desc=f"{symbol} {year}-{month:02d}-{day:02d}") as pbar:
 
-        file_name = build_filename(symbol, year, month, day, hour)
-        
-        open(file, "wb").write(data)
+            for f in as_completed(futures):
+                f.result()
+                pbar.update(1)
 
-    #extract_zip(data, dir)
-
-    print(f"[SUCCESSFUL] Fetched data saved to {dir}")
+    #print(f"[DONE] Completed {year}-{month:02d}-{day:02d}")
 
 
+def process_download(symbol, year, month, day, output_dir, threads):
+
+    download_day_data(symbol, year, month, day, output_dir, threads)
+
+    
 def parse_args():
     
     parser = argparse.ArgumentParser(description="Dukascopy Data Downloader")
@@ -98,21 +130,49 @@ def parse_args():
     parser.add_argument("--start-date", type=str, help="Start date in YYYY-MM-DD format")
     parser.add_argument("--end-date", type=str, help="End date in YYYY-MM-DD format")
 
+    #Threads
+    parser.add_argument(
+        "--threads",
+        type=int,
+        default=4,
+        help="Number of parallel download threads to use"
+    )
+
+    #Folder name
+    parser.add_argument(
+        "--location",
+        type=str,
+        default="bi5_data",
+        help="Specify directory/location to store data in"
+    )
+    
     return parser.parse_args()
 
 
-if __name__ == "__main__":
+def main():
     
     args = parse_args()
 
+    print("[BEGIN] Download requested data from Dukascopy.")
+          
     #Single day mode
     if args.year and args.month and args.day:
 
-        download_day(
+        output_dir = os.path.join(
+                args.location,
+                args.symbol,
+                f"{args.year}-{args.month:02d}-{args.day:02d}"
+            )
+
+        os.makedirs(output_dir, exist_ok=True)
+    
+        process_download(
             symbol=args.symbol,
             year=args.year,
             month=args.month,
-            day=args.day
+            day=args.day,
+            output_dir=output_dir,
+            threads=args.threads
         )
 
     #Range mode
@@ -124,16 +184,34 @@ if __name__ == "__main__":
         current_date = start_date
 
         while current_date <= end_date:
-            
-            download_day(
+
+            output_dir = os.path.join(
+                args.location,
+                symbol,
+                f"{year}-{month:02d}-{day:02d}"
+            )
+
+            os.makedirs(output_dir, exist_ok=True)
+    
+            process_download(
                 symbol=args.symbol,
                 year=current_date.year,
                 month=current_date.month,
-                day=current_date.day
+                day=current_date.day,
+                output_dir=output_dir,
+                threads=args.threads
             )
 
             current_date += timedelta(days=1)
-
+            
     else:
         
         print("[ERROR] Provide either --date or both --start-date and --end-date")
+
+        return 
+
+    print(f"[END] Fetched data saved to directory named {args.location}.")
+
+
+if __name__ == "__main__":
+    main()
